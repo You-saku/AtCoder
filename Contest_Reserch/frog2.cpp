6d6ada5cfc638dae3da7da39e30bd9d7d4dc214b
You-saku <usakusekine@gmail.com>
2021-01-27 00:19:24 +0900

    Additional(20210127)

--- Contest_Reserch/frog2.cpp
+++ Contest_Reserch/frog2.cpp
@@ -1,9 +1,23 @@
 #include<iostream>
 #include<vector>
+#include<cmath>
+#include<algorithm>
 
 using namespace std;
 
 int main() {
+
+    int n;
+    int k;
+    cin >> n >> k;
+    vector<int>h(n);
+    //配列定義は結構馬鹿にできない
+    int dp[100010];
+    dp[0] = 0;
     
+    for(int i = 0; i < n; i++){
+        cin >> h[i];
+    }
+
     return 0;
 }
