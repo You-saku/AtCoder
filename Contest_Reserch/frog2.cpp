9b9dbeced3cfaf09402aaaf8cf0c28ab989fb7c7
You-saku <usakusekine@gmail.com>
2021-01-27 17:25:09 +0900

    Additional(20210127)

--- Contest_Reserch/frog2.cpp
+++ Contest_Reserch/frog2.cpp
@@ -12,12 +12,29 @@ int main() {
     cin >> n >> k;
     vector<int>h(n);
     //配列定義は結構馬鹿にできない
-    int dp[100010];
+    long long int dp[100010];
+    for(int i = 0; i < n+10; i++){
+        dp[i] = 100000000000;
+    }
     dp[0] = 0;
     
     for(int i = 0; i < n; i++){
         cin >> h[i];
     }
+    
+
+    for(int i = 0; i < n; i++){
+        for(int j = 1; j<=k; j++){
+            if(i+j<n){
+                dp[i+j] = min(dp[i+j],dp[i]+abs(h[i]-h[i+j]));
+            }
+        }
+    }
+
+    for(int i = 1; i < n; i++){
+        cout << dp[i] << endl;
+    }
+    
 
     return 0;
 }
