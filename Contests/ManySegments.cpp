aaba26355686d9858f2306226a86a4d709bc4784
Yusaku Sekine <yusakusekine@YusakunoMacBook-Air.local>
2021-06-29 01:23:19 +0900

    20210629_コンテスト復習

--- Contests/ManySegments.cpp
+++ Contests/ManySegments.cpp
@@ -3,20 +3,37 @@
 using namespace std;
 
 int main(){
-    int sum = 0;
+    int count = 0;
     int n;
     cin >> n;
-
-    int xy[1000000000][1000000000];
+    int t;
+    int l[2010];
+    int r[2010];
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < 3; j++){
-            cin >> xy[i][j];
+        cin >> t >> l[i] >> r[i];
+        if(t==2){
+            l[i] = 2*l[i];
+            r[i] = 2*r[i]-1;
+        }
+        if(t == 3){
+            l[i] = 2*l[i]+1;
+            r[i] = 2*r[i];
+        }
+        if(t == 4){
+            l[i] = 2*l[i]+1;
+            r[i] = 2*r[i]-1;
+        }
+        if(t==1){
+            l[i] = 2*l[i];
+            r[i] = 2*r[i];
         }
     }
 
-    
-
-
-
+    for(int i = 0; i <= n-1; i++){
+        for(int j = i+1; j < n; j++){
+            if(max(l[i],l[j]) <= min(r[i],r[j])) count++;
+        }
+    }
+    cout << count << endl;
     return 0;
 }
---
+++ typical90/YokanParty.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n,l,k;
+    cin >> n >> l;
+    cin >> k;
+    int A[100000];
+    for(int i=0; i<n; i++){
+        cin >> A[i];
+    }
+    
+    return 0;
+}
